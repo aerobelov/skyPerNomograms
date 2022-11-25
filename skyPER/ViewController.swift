3594563b9856f09499ff567f2f553a56fb9e6742
Andrei <70259887+panishev95@users.noreply.github.com>
2022-11-25 14:48:26 +0300

    Initial Commit

---
+++ skyPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  skyPER
+//
+//  Created by Andrei Panishev on 25.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
